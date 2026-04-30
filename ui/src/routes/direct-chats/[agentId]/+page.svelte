e78783172570488b827281b664961aa4900921fd
jade <jade@hopepunk.me>
2026-04-30 14:33:48 +0400

    Direct chat navbar link applies name ellipsis

--- ui/src/routes/direct-chats/[agentId]/+page.svelte
+++ ui/src/routes/direct-chats/[agentId]/+page.svelte
@@ -70,6 +70,7 @@
 	import { longpress } from '$lib/actions/longpress';
 	import { isWideScreen } from '$lib/stores/screen.svelte';
 	import Avatar from '$lib/components/profiles/Avatar.svelte';
+	import AvatarWithName from '$lib/components/profiles/AvatarWithName.svelte';
 	let agentId = page.params.agentId!;
 
 	const contactsStore: ContactsStore = getContext('contacts-store');
@@ -490,7 +491,7 @@
 					{#if searchMode}
 						<Navbar
 							transparent={true}
-							titleClass="opacity1 w-full"
+							titleClass="opacity1 w-full min-w-0"
 							centerTitle={false}
 						>
 							{#snippet left()}
@@ -513,7 +514,7 @@
 					{:else}
 						<Navbar
 							transparent={true}
-							titleClass="opacity1 w-full"
+							titleClass="opacity1 w-full min-w-0"
 							centerTitle={false}
 						>
 							{#snippet left()}
@@ -527,18 +528,14 @@
 							{#snippet title()}
 								{#if profile}
 									<Link
-										class="flex items-center justify-start gap-2"
+										class="flex w-full min-w-0 items-center justify-start"
 										href={`/direct-chats/${agentId}/chat-settings`}
 										data-testid="direct-chat-settings-link"
 									>
-										<Avatar
-											image={profile!.avatar}
-											initials={profile!.name.slice(0, 2)}
-											style="--size: 2.5rem"
+										<AvatarWithName
+											{profile}
+											nameTestId="direct-chat-peer-name"
 										/>
-										<span data-testid="direct-chat-peer-name"
-											>{fullName(profile!)}</span
-										>
 									</Link>
 								{/if}
 							{/snippet}
