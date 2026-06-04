badc4cb93d9553515c1c31b80f48eed16145c373
guillem.cordoba <guillem.cordoba@gmail.com>
2026-06-04 19:33:20 +0200

    Bring back groupdetails

--- crates/dashchat-node/src/payload.rs
+++ crates/dashchat-node/src/payload.rs
@@ -102,6 +102,8 @@ pub enum ChatPayload {
 
     Reaction(ChatReaction),
 
+    GroupDetails(GroupDetails),
+
     /// Used to tell other group members about agents they may not know about
     /// (typically because they aren't contacts), so they can subscribe to those
     /// agents' announcements topics and see their profiles. The inviter publishes
