bb2ceacfcb34f8bba528791f0fb3c7d190728555
Guillem Cordoba <guillem.cordoba@gmail.com>
2026-06-04 13:23:43 +0200

    Add function to header

--- src-tauri/gen/apple/PushNotificationsExtension/PushNotifications-Bridging-Header.h
+++ src-tauri/gen/apple/PushNotificationsExtension/PushNotifications-Bridging-Header.h
@@ -20,3 +20,4 @@ void notification_destroy(struct push_notification *data);
 RustByteSlice notification_title(const struct push_notification *data);
 RustByteSlice notification_body(const struct push_notification *data);
 RustByteSlice notification_route(const struct push_notification *data);
+RustByteSlice notification_large_icon_bytes(const struct push_notification *data);
