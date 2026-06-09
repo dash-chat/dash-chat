2d88cd1fa17a603a7cfdacfbccf36af8cdb1c73d
guillem.cordoba <guillem.cordoba@gmail.com>
2026-06-09 05:43:09 +0200

    Point to IP address for the push notifications server

--- nix/servers.nix
+++ nix/servers.nix
@@ -50,7 +50,7 @@ let
         wants = [ "network-online.target" ];
         serviceConfig = {
           ExecStart =
-            "${mailbox}/bin/mailbox-server --addr 0.0.0.0:80 --push-notifications-url https://push-notifications-server.production.dash-chat.dash-chat.garnix.me";
+            "${mailbox}/bin/mailbox-server --addr 0.0.0.0:80 --push-notifications-url http://65.108.254.103";
           Restart = "always";
         };
       };
