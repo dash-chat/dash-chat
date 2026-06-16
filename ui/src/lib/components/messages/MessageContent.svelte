8ab35abd9941a89dcbeff63f176d8f3f6c276ed0
guillem.cordoba <guillem.cordoba@gmail.com>
2026-06-16 10:40:43 +0200

    Merge

--- ui/src/lib/components/messages/MessageContent.svelte
+++ ui/src/lib/components/messages/MessageContent.svelte
@@ -1,42 +1,59 @@
 <script lang="ts">
 	import type { Snippet } from 'svelte';
+	import type { Message } from 'dash-chat-stores';
 	import { highlightMatch } from './message-helpers';
 	import { shrinkToWidestLine } from '$lib/actions/shrink-to-widest-line';
+	import MessageAttachment from './MessageAttachment.svelte';
 
 	let {
-		content,
+		message,
 		searchQuery,
 		metadata,
+		senderName = '',
+		withContentAbove = false,
 	}: {
-		content: string;
+		message: Message;
 		searchQuery: string;
 		metadata?: Snippet;
+		senderName?: string;
+		withContentAbove?: boolean;
 	} = $props();
 
 	let metadataWidth = $state(0);
 </script>
 
-<div class="relative px-1">
-	{#if metadata}
-		<div
-			class="absolute bottom-0 end-0 flex items-center gap-1 whitespace-nowrap select-none"
-			bind:clientWidth={metadataWidth}
-		>
-			{@render metadata()}
+{#if message.content.media}
+	<MessageAttachment
+		media={message.content.media}
+		{withContentAbove}
+		withContentBelow={!!message.content.message || !!metadata}
+		{senderName}
+		timestamp={message.timestamp}
+	/>
+{/if}
+{#if message.content.message || metadata}
+	<div class="relative px-1">
+		{#if metadata}
+			<div
+				class="absolute bottom-0 end-0 flex items-center gap-1 whitespace-nowrap select-none"
+				bind:clientWidth={metadataWidth}
+			>
+				{@render metadata()}
+			</div>
+		{/if}
+		<div class="max-w-full" use:shrinkToWidestLine>
+			<span class="whitespace-pre-wrap">
+				{#if searchQuery}
+					{@html highlightMatch(message.content.message, searchQuery)}
+				{:else}
+					{message.content.message}
+				{/if}
+			</span>
+			<!-- Reserves the metadata's space in the bottom-end corner, since
+			     wrapped text cannot be made to avoid an absolute box via CSS. -->
+			{#if metadata}
+				<span class="ms-2.5 inline-block" style="width: {metadataWidth}px"></span>
+			{/if}
 		</div>
-	{/if}
-	<div class="max-w-full" use:shrinkToWidestLine>
-		<span class="whitespace-pre-wrap"
-			>{#if searchQuery}{@html highlightMatch(
-					content,
-					searchQuery,
-				)}{:else}{content}{/if}</span
-		>
-		<!-- Reserves the metadata's space in the bottom-end corner, since
-		     wrapped text cannot be made to avoid an absolute box via CSS. -->
-		{#if metadata}<span
-				class="ms-2.5 inline-block"
-				style="width: {metadataWidth}px"
-			></span>{/if}
 	</div>
-</div>
+{/if}
