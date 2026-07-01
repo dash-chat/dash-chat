f36ae9273ce00601f795b3b74d102a38c2a9d0af
guillem.cordoba <guillem.cordoba@gmail.com>
2026-07-01 13:46:43 +0200

    Better debug

--- crates/mailbox-server/src/main.rs
+++ crates/mailbox-server/src/main.rs
@@ -25,7 +25,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     tracing_subscriber::registry()
         .with(
             tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| {
-                "mailbox_server=debug,dashchat_utils=debug,iroh=debug,iroh_blobs=debug".into()
+                "mailbox_server=debug,dashchat_utils=debug,iroh=info,iroh_blobs=info".into()
             }),
         )
         .with(tracing_subscriber::fmt::layer())
