551a5dc1665d582c6b7d2a622fc8d90f6288dc4d
jade <jade@hopepunk.me>
2026-07-17 14:39:52 +1000

    handle slashes better through refactoring

--- ui/src/lib/deep-links/add-contact.ts
+++ ui/src/lib/deep-links/add-contact.ts
@@ -1,26 +1,16 @@
 import { goto } from '$app/navigation';
 
-import {
-	HTTPS_DEEP_LINK_BASE_URL,
-	SCHEME_DEEP_LINK_BASE_URL,
-} from './constants';
+import { buildHttpsDeepLinkUrl, extractDeepLinkParams } from './helpers';
 
 export const path = '/add-contact/{{code}}';
 
-const pathPrefix = path.replace('{{code}}', '');
-const httpsDeepLinkPrefix = HTTPS_DEEP_LINK_BASE_URL + pathPrefix;
-const schemeDeepLinkPrefix = SCHEME_DEEP_LINK_BASE_URL + pathPrefix.slice(1);
-
-export function toDeepLink(code: string): string {
-	return httpsDeepLinkPrefix + code;
+export function toDeepLink(code: string): string | null {
+	return buildHttpsDeepLinkUrl(path, { code });
 }
 
-export function extractCodeFromDeepLink(input: string): string {
-	if (input.startsWith(httpsDeepLinkPrefix))
-		return input.slice(httpsDeepLinkPrefix.length);
-	if (input.startsWith(schemeDeepLinkPrefix))
-		return input.slice(schemeDeepLinkPrefix.length);
-	return input;
+export function extractCodeFromDeepLink(input: string): string | null {
+	let params = extractDeepLinkParams(input, path);
+	return params?.code ?? null;
 }
 
 export function handle({ code }: Record<string, string>) {
--- ui/src/lib/deep-links/constants.ts
+++
@@ -1,2 +0,0 @@
-export const HTTPS_DEEP_LINK_BASE_URL = 'https://dashchat.org';
-export const SCHEME_DEEP_LINK_BASE_URL = 'dash-chat:/';
