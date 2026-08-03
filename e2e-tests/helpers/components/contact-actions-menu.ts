2394361384fb68dbbdd42e3c14f58651aa5809e2
guillem.cordoba <guillem.cordoba@gmail.com>
2026-08-03 12:54:18 +0200

    Remove unblock branch from ContactActionsMenu

--- e2e-tests/helpers/components/contact-actions-menu.ts
+++ e2e-tests/helpers/components/contact-actions-menu.ts
@@ -1,11 +1,9 @@
 import { TestHelper } from '../pages/test-helper';
 import { tid } from '../selectors';
 
-/** The contact overflow popover and the block/unblock dialogs it opens. */
+/** The contact overflow popover and the block dialog it opens. */
 export class ContactActionsMenu extends TestHelper {
 	menu = this.el(tid('contact-actions-menu'));
 	block = this.el(tid('contact-block'));
-	unblock = this.el(tid('contact-unblock'));
 	blockConfirm = this.el(tid('block-contact-confirm'));
-	unblockConfirm = this.el(tid('unblock-contact-confirm'));
 }
