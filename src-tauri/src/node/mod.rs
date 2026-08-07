006602b3ffab3de8c5204ad287e92f8fd49c02d4
jade <jade@hopepunk.me>
2026-08-07 10:44:48 +1000

    fix unexported NodeContext

--- src-tauri/src/node/mod.rs
+++ src-tauri/src/node/mod.rs
@@ -4,3 +4,6 @@ mod node_context;
 pub mod node_slot;
 
 pub use app_node_manager::AppNodeManager;
+
+#[cfg_attr(not(target_os = "android"), allow(unused_imports))]
+pub use node_context::NodeContext;
