4c26965bbe3631f65866b00e1bcfef0daef94377
guillem.cordoba <guillem.cordoba@gmail.com>
2026-08-22 16:26:07 +0200

    Removed report from contact actions menu helper

--- e2e-tests/helpers/components/contact-actions-menu.ts
+++ e2e-tests/helpers/components/contact-actions-menu.ts
@@ -1,11 +1,9 @@
 import { TestHelper } from '../pages/test-helper';
 import { tid } from '../selectors';
 
-/** The contact overflow popover and the block/report dialogs it opens. */
+/** The contact overflow popover and the block dialog it opens. */
 export class ContactActionsMenu extends TestHelper {
 	menu = this.el(tid('contact-actions-menu'));
 	block = this.el(tid('contact-block'));
 	blockConfirm = this.el(tid('block-contact-confirm'));
-	report = this.el(tid('contact-report'));
-	reportConfirm = this.el(tid('report-contact-confirm'));
 }
