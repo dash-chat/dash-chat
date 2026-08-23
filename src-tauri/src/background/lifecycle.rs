4be3bb7d7c8adbc33a0b149e2101bd1e865520fa
jade <jade@hopepunk.me>
2026-08-23 16:17:19 +0100

    change notification text

--- src-tauri/src/background/lifecycle.rs
+++ src-tauri/src/background/lifecycle.rs
@@ -1,5 +1,5 @@
-use tauri::{AppHandle, Wry};
 use tauri::Manager;
+use tauri::{AppHandle, Wry};
 use tauri_plugin_background_service::ServiceManagerHandle;
 
 pub(crate) async fn on_pause(app: AppHandle<Wry>) {
@@ -8,7 +8,9 @@ pub(crate) async fn on_pause(app: AppHandle<Wry>) {
     }
     let manager = app.state::<ServiceManagerHandle<Wry>>();
     let config = tauri_plugin_background_service::StartConfig {
-        service_label: "Dash Chat".into(),
+        service_label:
+            "Dash Chat is running in the background, to ensure you receive messages while offline."
+                .into(),
         foreground_service_type: "remoteMessaging".into(),
     };
     if let Err(e) = manager.start(app.clone(), config).await {
