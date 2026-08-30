5e9f61e96862a6171b3276b4c2ba25a523f750e1
guillem.cordoba <guillem.cordoba@gmail.com>
2026-08-31 01:05:39 +0200

    Bumped database version

--- src-tauri/src/filesystem.rs
+++ src-tauri/src/filesystem.rs
@@ -1,7 +1,7 @@
 use std::path::PathBuf;
 use tauri::{AppHandle, Manager, Runtime};
 
-const DATABASE_VERSION: &str = "0.11";
+const DATABASE_VERSION: &str = "0.12";
 
 /// When `DATA_DIR` is set, isolate the XDG directories under it so concurrently
 /// running desktop instances don't share WebKitGTK's SQLite databases (which
