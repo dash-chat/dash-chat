dd4c73dc34a6640d0b4f4064448e0a4c50be7192
guillem.cordoba <guillem.cordoba@gmail.com>
2026-08-30 17:30:05 +0200

    Remove local message server title

--- ui/src/routes/settings/offline/+page.svelte
+++ ui/src/routes/settings/offline/+page.svelte
@@ -72,7 +72,6 @@
 	<div class="column" style="flex: 1">
 		{#if !isMobile}
 			<div class="column center-in-desktop">
-				<BlockTitle>{m.localMessageServer()}</BlockTitle>
 				<List strongIos inset={isWideScreen.value || theme === 'ios'}>
 					<ListItem
 						title={m.enableLocalMessageServer()}
